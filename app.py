0449962ef541a888fcb0b23656df54b3e12541fc
Vallabhuhemanth <vallabhuhemanth8@gmail.com>
2026-03-29 23:19:47 +0530

    Added admin page and login dashboard to see the messages

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, session
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
+# secret key for session
+app.secret_key='admin_secret_123'
+
+# mysql config
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = '970056'
 app.config['MYSQL_DB'] = 'pos_billing'
 
 mysql=MySQL(app)
+
+# Admin credentials
+ADMIN_USERNAME = 'admin'
+ADMIN_PASSWORD = 'admin123'
+
 
 @app.route('/')
 def home():
@@ -29,6 +38,40 @@ def contact():
     cursor.close()
 
     return render_template('index.html', success=True)
+
+# amdin login route
+@app.route('/admin', methods=['GET', 'POST'])
+def admin_login():
+    error = None
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        if username == ADMIN_USERNAME and password == ADMIN_PASSWORD:
+            session['admin']= True
+            return redirect(url_for('admin_dashboard'))
+        else:
+            error = 'Invalid Username or Password!'
+    return render_template('login.html', error=error)
+
+# Admin dashboard route
+@app.route('/admin/dashboard')
+def admin_dashboard():
+    if not session.get('admin'):
+        return redirect(url_for('login.html'))
+    
+    cursor = mysql.connection.cursor()
+    cursor.execute("SELECT * FROM contacts ORDER BY created_at DESC")
+    messages = cursor.fetchall()
+    cursor.close()
+
+    return render_template('admin.html',messages=messages)
+
+# Logout Route
+@app.route('/logout')
+def logout():
+    session.pop('admin', None)
+    return redirect(url_for('admin_login'))
 
 if __name__ == '__main__':
     app.run(debug=True)
